16a9938b94f7758468edcc1f9a88c022113d5ab8
Pushkar Raj <pushkarraj641996@gmail.com>
2018-02-11 20:00:13 +0530

    single change

--- app/src/main/java/com/example/pushkarraj/calculator/MainActivity.java
+++ app/src/main/java/com/example/pushkarraj/calculator/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
     public void add(View view){
 
         Intent intent = new Intent(this, Add.class);
